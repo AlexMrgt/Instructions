d34b7aa11f8d8bc7c26392c65346bf70dfcf52a5
AlexMrgt <git.mrgt@gmail.com>
2022-05-05 21:05:42 +0500

    add APPROVED

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,14 +2,20 @@ import './Main.css';
 
 import Questions from '../Questions/Questions';
 import ItemsForm from '../ItemsForm/ItemsForm';
- 
+
+import ApprovedImage from '../../images/approved.png';
+
 
 const Main = () => {
 
-  
+
 
     return (
         <main className='main'>
+
+            <div className='main__approved-container' >
+                <img src={ApprovedImage} alt='approved' className='main__approved-image'></img>
+            </div>
 
             <h1 className='main__title'>ФАК ( за мат извени )</h1>
 
